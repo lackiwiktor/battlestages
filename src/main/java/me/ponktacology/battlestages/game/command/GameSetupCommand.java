a480676f9813734b49d9d472c17a7aef7d2a4e89
ponktacology <ponktacology@mailfence.com>
2020-10-18 14:13:28 +0200

    better command description

--- src/main/java/me/ponktacology/battlestages/game/command/GameSetupCommand.java
+++ src/main/java/me/ponktacology/battlestages/game/command/GameSetupCommand.java
@@ -18,7 +18,7 @@ public class GameSetupCommand {
 
     private final Game game;
 
-    @Command(name = "setGameLoc", desc = "Sets game location")
+    @Command(name = "setGameLoc", desc = "Sets game location.")
     @Require("game.manage")
     public void setSpawn(@Sender Player player) {
         Game.GAME_LOCATION = player.getLocation();
@@ -26,7 +26,7 @@ public class GameSetupCommand {
         player.sendMessage(ColorUtil.color("&aSuccessfully changed game location."));
     }
 
-    @Command(name = "setWaitingLoc", desc = "Sets game waiting location")
+    @Command(name = "setWaitingLoc", desc = "Sets game waiting location.")
     @Require("game.manage")
     public void setWaiting(@Sender Player player) {
         Game.WAITING_LOCATION = player.getLocation();
@@ -34,7 +34,7 @@ public class GameSetupCommand {
         player.sendMessage(ColorUtil.color("&aSuccessfully changed game waiting location."));
     }
 
-    @Command(name = "forceStart", desc = "Forces game to start")
+    @Command(name = "forceStart", desc = "Forces game to start.")
     @Require("game.manage")
     public void forceStart(@Sender CommandSender sender) {
         if (game.getState() == GameState.WAITING_FOR_PLAYERS) {
@@ -47,7 +47,7 @@ public class GameSetupCommand {
         sender.sendMessage(ColorUtil.color("&aSuccessfully force started game."));
     }
 
-    @Command(name = "forceEnd", desc = "Forces game to end")
+    @Command(name = "forceEnd", desc = "Forces game to end.")
     @Require("game.manage")
     public void forceEnd(@Sender CommandSender sender) {
         if (game.getState() == GameState.RUNNING) {
@@ -60,7 +60,7 @@ public class GameSetupCommand {
         sender.sendMessage(ColorUtil.color("&aSuccessfully force ended game."));
     }
 
-    @Command(name = "addItem", desc = "Adds item")
+    @Command(name = "addItem", desc = "Adds item to possible kill rewards.")
     @Require("game.manage")
     public void addItem(@Sender Player sender) {
         Game.ITEMS.add(sender.getInventory().getItemInMainHand());
@@ -68,7 +68,7 @@ public class GameSetupCommand {
         sender.sendMessage(ColorUtil.color("&aSuccessfully added item."));
     }
 
-    @Command(name = "removeItem", desc = "Removes an item", usage = "<index>")
+    @Command(name = "removeItem", desc = "Removes an item from possible kill rewards.", usage = "<index>")
     @Require("game.manage")
     public void removeItem(@Sender CommandSender sender, Integer index) {
         if (Game.ITEMS.remove(index.intValue()) != null) {
@@ -78,7 +78,7 @@ public class GameSetupCommand {
         }
     }
 
-    @Command(name = "listItems", desc = "Prints list of items.")
+    @Command(name = "listItems", desc = "Prints list of possible kill rewards.")
     @Require("game.manage")
     public void listItems(@Sender CommandSender sender) {
         List<ItemStack> items = Game.ITEMS;
